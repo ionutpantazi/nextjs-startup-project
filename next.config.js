82bc09ed3f843fdbf97f452e189c6aa2c1d7cf75
ionut <ionut@vocala.co>
2024-05-28 15:29:16 +0100

    use strapi image

--- next.config.js
+++ next.config.js
@@ -1,14 +1,10 @@
-const IMAGE_DOMAINS = process.env.NEXT_PUBLIC_IMAGE_DOMAINS
-  ? process.env.NEXT_PUBLIC_IMAGE_DOMAINS?.split(',').map((i) => new URL(i).host)
-  : []
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   compiler: {
     styledComponents: true
   },
   images: {
-    domains: ['lg-prototype-strapi.azurewebsites.net', 'img.youtube.com'],
+    domains: ['lg-strapi-s3.s3.us-east-1.amazonaws.com', 'img.youtube.com'],
     deviceSizes: [640, 768, 1024, 1280, 1536]
   }
 }
